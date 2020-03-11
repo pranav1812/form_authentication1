5a4746377fd7be0d5edcd2f76f9a7b0adf52d0fe
pranav1812 <pranavvohra1812@yahoo.in>
2020-03-11 14:13:07 +0530

    insecure authentication without tokens

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ mongoose.connect('mongodb://localhost/playground',{useNewUrlParser:true,  useUni
     .then(()=>console.log('connected'))
 
 const courseSchema=mongoose.Schema({
-    email: String,
+    email: {type:String, required:true, unique: true},
     password: String,
     username:String,
     dob:String
@@ -21,6 +21,10 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/form.html");
 });
 
+app.get("/login",(req,res)=>{
+    res.sendFile(__dirname+"/login.html")
+})
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -32,7 +36,7 @@ app.post('/form',(req,res)=>{
     myData.save()
         .then(item=> {
             console.log(item);
-            Course.find()
+            Course.find({username:"abcxyz"})
                 .then((courses=> console.log(courses)))
         })
         .catch(err=>{
@@ -42,5 +46,18 @@ app.post('/form',(req,res)=>{
 
 })
 
+app.post('/login',(req,res)=>{
+    //res.send(req.body);
+    Course.findOne({ email:req.body.email},(err,course)=>{
+        if(err|| !course || req.body.password!=course.password){
+            console.log('wrong username / password');
+            res.sendFile(__dirname+'/login.html')
+        }
+        else{
+            res.sendFile(__dirname+'/dashboard.html')
+        }
+    })
+
+})
 
 app.listen(3000)
